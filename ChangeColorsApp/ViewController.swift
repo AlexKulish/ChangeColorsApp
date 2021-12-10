4e305d46b4deaec071781025f1da2baedd8b4d88
Alexey Kulish <alexkulish1993@gmail.com>
2021-12-10 17:41:48 +0300

    Setup sliders and values of sliders

--- ChangeColorsApp/ViewController.swift
+++ ChangeColorsApp/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var valueRedLabel: UILabel!
+    @IBOutlet var valueGreenLabel: UILabel!
+    @IBOutlet var valueBlueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+        
+        valueRedLabel.text = String(redSlider.value)
+        valueGreenLabel.text = String(greenSlider.value)
+        valueBlueLabel.text = String(blueSlider.value)
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        valueRedLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
 
